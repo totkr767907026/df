c6e022c2c107b78f6b59c82172f25d19dc35408e
pyt(ToTkr2) <58772909+totkr767907026@users.noreply.github.com>
2019-12-25 19:39:52 +0800

    Update ReplyMapper.java

--- src/main/java/com/fc/mapper/ReplyMapper.java
+++ src/main/java/com/fc/mapper/ReplyMapper.java
@@ -20,3 +20,17 @@ public interface ReplyMapper {
     String getContentByRid(int rid);
 
 }
+
+String name, email, subject, content, time;
+                           int reply, id;
+                           while (rs.next()) {
+                                  name = rs.getString(1);
+                                  email = rs.getString(2);
+                                  subject = rs.getString(3);
+                                  content = rs.getString(4);
+                                  time = rs.getString(5);
+                                  id = rs.getInt(7);
+                                  out.print("<tr bgcolor=yellow><td><a href="delete.jsp?id=" mce_href="delete.jsp?id=""+id+"&replyid=0><img src="Pictures/delete.bmp" mce_src="Pictures/delete.bmp" width=20 height=20></a></td><td width=300><a href="detail.jsp?id=" mce_href="detail.jsp?id=""+ id + ">" + subject + "</a></td>");
+                                  out.print("<td width=200>" + name + "</td>");
+                                  out.print("<td width=200>" + time + "</td></tr>");
+                           }
